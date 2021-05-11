7ec1099c5e3e27b87cc857e6345f821a3130429b
Yurii Rashkovskii <yrashk@gmail.com>
2021-05-11 14:14:46 -0700

    Problem: relayed request doesn't contain original headers
    This prevents Gopkg from collecting access intelligence (user-agent, original IP address, etc.)
    
    Solution: pass headers to the request made to Gopkg

--- worker.js
+++ worker.js
@@ -6,7 +6,9 @@ function handleRequest(req) {
     const url = new URL(req.url)
     const params = new URLSearchParams(url.search)
     if (params.get('go-get') == "1") {
-        return fetch("https://gopkg.co" + url.pathname + "?go-get=1")
+        return fetch("https://gopkg.co" + url.pathname + "?go-get=1", {
+            headers: req.headers
+        })
     } else {
         return fetch(req) 
     }
